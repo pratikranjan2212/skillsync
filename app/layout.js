0f9c233bbffe0b048557fd43659c5421b4d0a552
SkillSync Developer <developer@skillsync.local>
2026-08-16 14:14:57 +0530

    Fix React hydration mismatch error by adding suppressHydrationWarning to html tag in RootLayout

--- app/layout.js
+++ app/layout.js
@@ -25,6 +25,7 @@ export default function RootLayout({ children }) {
     <html
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+      suppressHydrationWarning
     >
       <body
         className="min-h-full flex flex-col bg-[#F5F5F3] text-[#111111]"
